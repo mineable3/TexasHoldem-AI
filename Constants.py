223f5f12f3b7e15d6e47081d0350f40fd77743e2
Emmet Spaeth <emmet.spaeth@gmail.com>
2023-09-10 18:37:37 -0400

    Added comments explaining some Constants

--- Constants.py
+++ Constants.py
@@ -3,13 +3,24 @@
 class Constants:
 
 
-    #AI
+#AI=======================================================
+    
+    #the absolute value percent a weight can mutate by
     factorOfMutation = .015
+
+    #the number of decimals a weight should have
     precisionOfWeights = 3
+
+    #a number to make the outputs either bigger or smaller like scientific notation
     outputScaleFactor = .001
+
+    #the number of decimals in the ai's output
     outputPrecicion = 0
 
-    #game
+    #time in minutes
+    timeToTrain = 1
+
+#game======================================================
     startingCash = 500
     ante = 10
     bigBlind = 30
